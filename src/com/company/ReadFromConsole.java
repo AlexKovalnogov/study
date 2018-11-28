830e522d3ba7350aca16cf0e7b8a4e33077ea7ff
Oleksii Kovalnohov <oleksii.kovalnohov@worldapp.com>
2018-11-28 14:26:54 +0200

    Changed label Input your data toInput your data1

--- src/com/company/ReadFromConsole.java
+++ src/com/company/ReadFromConsole.java
@@ -7,7 +7,7 @@ public class ReadFromConsole {
     static  int ConsInptRead()
     {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Input your data: ");
+        System.out.print("Input your data:1 ");
         int input = scanner.nextInt();
        // scanner.close();
 return input;
